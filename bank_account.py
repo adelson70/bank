e0d5f7b9e247237ebb4e9a9c0936074641f133c5
adelson70 <129080230+adelson70@users.noreply.github.com>
2024-03-09 20:08:41 -0300

    sem gui kkkkk

--- bank_account.py
+++ bank_account.py
@@ -1,16 +1,18 @@
 class Account():
 
-    def __init__(self, id, cpf, cc):
+    def __init__(self, id, cpf, cc, senha):
         self.id = id
         self.cpf = cpf
         self.saldo = 0
         self.conta_corrente = cc
+        self.password = senha
 
     def enviar_dinheiro(self, quantia):
         if quantia > self.saldo:
             return 0
         else:
             self.saldo -= quantia
+            return True
 
     def receber_dinheiro(self, quantia):
         self.saldo += quantia
@@ -20,3 +22,15 @@ class Account():
             return 0
         else:
             self.saldo -= quantia
+            return True
+
+    def alterar_senha(self, nova_senha):
+        if nova_senha == self.password:
+            return 2
+        
+        elif len(nova_senha) < 8:
+            return 0
+        
+        else:
+            self.password = nova_senha
+            return 1
